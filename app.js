801df2704a92d805e1bc994e70323e8ed6ca334e
Melany <mefrancom@unal.edu.co>
2024-07-14 18:47:11 -0500

    terminado

--- app.js
+++ app.js
@@ -1,32 +1,77 @@
+let reemplazo = ["e", "enter","o", "ober", "i", "imes","a", "ai", "u", "ufat"];
+
+function copiarTextoAlPortapapeles(texto) {
+    
+    navigator.clipboard.writeText(texto)
+        .catch(err => {
+            // Error: Failed to copy text
+            console.error('No se pudo copiar el texto', err);
+        });
+}
+
 function encriptar() {
     let texto = document.getElementById('input').value;
     
-    let encriptado = [];
+    let encriptado = texto;
+
+    for (let i = 0; i < 9; i +=2){
+        if(encriptado.includes(reemplazo[i])){
+            encriptado = encriptado.replaceAll(reemplazo[i], reemplazo[i+1]);
+        }
+    }
+    let nuevoParrafo = document.createElement('p');
+    let botonCopiar = document.createElement('button');
+    let division = document.createElement('div');
+
+    division.className = "nuevo-div";
+    botonCopiar.className = "copiar";
+    botonCopiar.textContent ="Copiar";
+    botonCopiar.onclick = () => copiarTextoAlPortapapeles(encriptado);
+    nuevoParrafo.classList.add('nuevo-parrafo');
+    nuevoParrafo.id = 'texto';
+    
+    
+    
+    nuevoParrafo.textContent = encriptado; 
+
+    division.appendChild(nuevoParrafo);
+    division.appendChild(botonCopiar);
+    let contenedorDer = document.querySelector('.der');
+    contenedorDer.innerHTML = '';
+    contenedorDer.appendChild(division);
+}
 
 
-    for (let letra of texto) {
-        
-        if(letra == "a"){
-            encriptado.push("ai");
-        } else if(letra == "e"){
-            encriptado.push("enter");
-        } else if (letra == "i"){
-            encriptado.push("imes");
-        } else if (letra == "o"){
-            encriptado.push("ober");
-        } else if (letra == "u"){
-            encriptado.push("ufat");
-        } else {
-            encriptado.push(letra);
+
+
+function desencriptar(){
+    let texto = document.getElementById('input').value;
+    let desencriptado = texto;
+    for(let i = 1; i < 10; i+= 2){
+        if(desencriptado.includes(reemplazo[i])){
+            desencriptado = desencriptado.replaceAll(reemplazo[i], reemplazo[i-1]);
         }
     }
 
     let nuevoParrafo = document.createElement('p');
-    
-    // Añadir el texto al nuevo elemento p
-    nuevoParrafo.textContent = encriptado.join(''); // Aquí puedes poner tu lógica de encriptación
+    let botonCopiar = document.createElement('button');
+    let division = document.createElement('div');
 
-    // Añadir el nuevo p al contenedor de la clase "der"
+    division.className = "nuevo-div";
+    botonCopiar.className = "copiar";
+    botonCopiar.textContent ="Copiar";
+    botonCopiar.onclick = () => copiarTextoAlPortapapeles(desencriptado);
+    nuevoParrafo.classList.add('nuevo-parrafo');
+    nuevoParrafo.id = 'texto';
+    
+    
+    
+    nuevoParrafo.textContent = desencriptado; 
+
+    division.appendChild(nuevoParrafo);
+    division.appendChild(botonCopiar);
     let contenedorDer = document.querySelector('.der');
-    contenedorDer.innerHTML = nuevoParrafo.textContent;
+    contenedorDer.innerHTML = '';
+    contenedorDer.appendChild(division);
+
 }
